fbee40261390d1fa0185dfee265f35bec8a29261
László Dániel <laszlodaniel@gmail.com>
2023-01-23 13:53:18 +0100

    Fix baudrate select bug in GUI
    Do not change baudrate setting after connection.

--- GUI/ChryslerScanner/MainForm.cs
+++ GUI/ChryslerScanner/MainForm.cs
@@ -3211,8 +3211,6 @@ namespace ChryslerScanner
 
                 Util.UpdateTextBox(USBTextBox, "[INFO] Device connected to " + SelectedPort + ".");
 
-                UpdateUARTBaudrate(Properties.Settings.Default.UARTBaudrate);
-
                 DeviceFound = true;
                 ConnectButton.Text = Languages.strings.Disconnect;
                 COMPortsComboBox.Enabled = false;
