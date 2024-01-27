a3315cd9354fffc2184db858f87f17ec622c4640
László Dániel <laszlodaniel@gmail.com>
2024-01-27 10:46:44 +0100

    Improve memory reading over CCD-bus

--- GUI/ChryslerScanner/Properties/AssemblyInfo.cs
+++ GUI/ChryslerScanner/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Windows.Media;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.15.6")]
+[assembly: AssemblyVersion("0.15.7")]
 //[assembly: AssemblyFileVersion("1.0.0")]
