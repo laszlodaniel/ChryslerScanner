79ec778df37af819f75703910ff4326732361758
László Dániel <laszlodaniel@gmail.com>
2024-09-22 08:50:42 +0200

    Adjust task stack sizes
    Add OTA update library to scanner firmware.
    
    Bump esp_littlefs version from 1.8.0 to 1.14.8
    Bump littlefs version from 2.7.0 to 2.9.3
    
    Modify partition table to include app1 ota partition for future wireless firmware update.
    
    Rename partition_table.bin to partitions.bin.

--- GUI/ChryslerScanner/Properties/AssemblyInfo.cs
+++ GUI/ChryslerScanner/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Windows.Media;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.16.3")]
+[assembly: AssemblyVersion("0.16.4")]
 //[assembly: AssemblyFileVersion("1.0.0")]
